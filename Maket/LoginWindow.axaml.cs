7cc5387dfb5744059f488ee120936102ed3d3e49
Максимус <mikhailcher2006@gmail.com>
2025-11-11 14:16:29 +0300

    Верстка авторизации

---
+++ Maket/LoginWindow.axaml.cs
@@ -0,0 +1,13 @@
+﻿using Avalonia;
+using Avalonia.Controls;
+using Avalonia.Markup.Xaml;
+
+namespace Maket;
+
+public partial class LoginWindow : Window
+{
+    public LoginWindow()
+    {
+        InitializeComponent();
+    }
+}
