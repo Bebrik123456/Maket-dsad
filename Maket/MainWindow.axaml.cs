199ca7aefff8825fbe915e8a68ac555ccc906d35
Максимус <mikhailcher2006@gmail.com>
2025-11-12 13:48:30 +0300

    для переноса

---
+++ Maket/MainWindow.axaml.cs
@@ -0,0 +1,13 @@
+﻿using Avalonia;
+using Avalonia.Controls;
+using Avalonia.Markup.Xaml;
+
+namespace Maket;
+
+public partial class MainWindow : Window
+{
+    public MainWindow()
+    {
+        InitializeComponent();
+    }
+}
